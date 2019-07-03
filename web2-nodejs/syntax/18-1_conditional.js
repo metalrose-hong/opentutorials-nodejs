24fb6f71b402b3c878dbc41fe106bd4551a6da66
metalrose.hong <metalrose.hong@gmail.com>
2019-07-03 22:43:22 +0900

    Add Chapter18

--- web2-nodejs/syntax/18-1_conditional.js
+++ web2-nodejs/syntax/18-1_conditional.js
@@ -1,8 +1,8 @@
 var args = process.argv;
-console.log(args[2]);
+console.log(args);
 console.log('A');
 console.log('B');
-if(args[2] === '1') {
+if(false) {
     console.log('C1');
 } else {
     console.log('C2');
--- web2-nodejs/syntax/18-2_conditional.js
+++ web2-nodejs/syntax/18-2_conditional.js
@@ -2,7 +2,7 @@ var args = process.argv;
 console.log(args[2]);
 console.log('A');
 console.log('B');
-if(args[2] === '1') {
+if(false) {
     console.log('C1');
 } else {
     console.log('C2');
