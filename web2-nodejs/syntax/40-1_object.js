f9f32450e35c1a54af6b681161f5b7474c74d2cc
metalrose.hong <metalrose.hong@gmail.com>
2019-08-14 15:35:51 +0900

    Add Chapter40

--- web2-nodejs/syntax/40-1_object.js
+++ web2-nodejs/syntax/40-1_object.js
@@ -1,4 +1,3 @@
-/*
 var members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]);
 
@@ -7,8 +6,8 @@ while(i < members.length) {
   console.log('array loop', members[i]);
   i = i + 1;
 }
-*/
 
+/*
 var roles = {
   'programmer':'egoing',
   'designer':'k8805',
@@ -16,7 +15,4 @@ var roles = {
 }
 console.log(roles.designer);
 console.log(roles['designer']);
-
-for(var name in roles) {
-  console.log('object => ', name, 'value => ', roles[name]);
-}
+*/
--- web2-nodejs/syntax/40-2_object.js
+++ web2-nodejs/syntax/40-2_object.js
@@ -18,5 +18,5 @@ console.log(roles.designer);
 console.log(roles['designer']);
 
 for(var name in roles) {
-  console.log('object => ', name, 'value => ', roles[name]);
+  console.log('object => ', name);
 }
