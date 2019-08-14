f534ddaad502e7bb01da01013a16bccfce35962a
metalrose.hong <metalrose.hong@gmail.com>
2019-08-14 19:45:53 +0900

    Modify Chapter38

--- web2-nodejs/38-1_main.js
+++ web2-nodejs/38-1_main.js
@@ -102,7 +102,7 @@ var app = http.createServer(function(request,response){
                 response.end();
             });
         });
-    } else if(pathname == 'update') {
+    } else if(pathname === '/update') {
         fs.readdir('./data', function(error, filelist) {
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 var title = queryData.id;
@@ -126,7 +126,7 @@ var app = http.createServer(function(request,response){
                 response.end(template);
             });
         });
-    } else if(pathname == '/update_process') {
+    } else if(pathname === '/update_process') {
         var body = "";
         request.on('data', function(data) {
             body = body + data;
@@ -143,7 +143,7 @@ var app = http.createServer(function(request,response){
                 });
             });
         });
-    } else if(pathname == '/delete_process') {
+    } else if(pathname === '/delete_process') {
         var body = "";
         request.on('data', function(data) {
             body = body + data;
@@ -152,7 +152,7 @@ var app = http.createServer(function(request,response){
             var post = qs.parse(body);
             var id = post.id;
             fs.unlink(`data/${id}`, function(error) {
-                response.writeHead(200);
+                response.writeHead(302, {Location: `/`});
                 response.end();
             });
         });
--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -57,7 +57,12 @@ var app = http.createServer(function(request,response){
                     var list = templateList(filelist);
                     var template = templateHTML(title, list,
                         `<h2>${title}</h2><p>${description}</p>`,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>`
                     );
                     response.writeHead(200);
                     response.end(template);
@@ -119,6 +124,36 @@ var app = http.createServer(function(request,response){
                 );
                 response.writeHead(200);
                 response.end(template);
+            });
+        });
+    } else if(pathname === '/update_process') {
+        var body = "";
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function(error) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                    response.writeHead(200);
+                    response.end('success');
+                });
+            });
+        });
+    } else if(pathname === '/delete_process') {
+        var body = "";
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(error) {
+                response.writeHead(302, {Location: `/`});
+                response.end();
             });
         });
     } else {
