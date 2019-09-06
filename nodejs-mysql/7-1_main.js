4110c76a5a36e9c19c55b22f7957c50a6a9288b4
metalrose.hong <metalrose.hong@gmail.com>
2019-09-06 21:20:07 +0900

    Add Chapter7

--- nodejs-mysql/7-1_main.js
+++ nodejs-mysql/7-1_main.js
@@ -136,11 +136,18 @@ var app = http.createServer(function(request, response) {
         });
         request.on('end', function() {
             var post = qs.parse(body);
-            db.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?',
-                [post.title, post.description, post.id], function(error, result) {
-                response.writeHead(302, {Location: `/?id=${post.id}`});
-                response.end();
-            });
+            db.query(`
+                INSERT INTO topic (title, description, created, author_id)
+                    VALUES(?, ?, NOW(), ?)`,
+                [post.title, post.description, 1],
+                function(error, result) {
+                    if(error) {
+                        throw error;
+                    }
+                    response.writeHead(302, {Location: `/?id=${result.insertId}`});
+                    response.end();
+                }
+            );
         });
     } else if(pathname === '/delete_process') {
         var body = '';
