b261d2b7c33f8d4272ad3d4c7c2d0e17022ea207
metalrose.hong <metalrose.hong@gmail.com>
2019-09-06 21:22:17 +0900

    Add Chapter9

--- nodejs-mysql/9-1_main.js
+++ nodejs-mysql/9-1_main.js
@@ -38,19 +38,15 @@ var app = http.createServer(function(request, response) {
                     throw error;
                 }
                 db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id
-                    WHERE topic.id=?`,[queryData.id], function(error2, topic) {
+                    WHERE id=?`,[queryData.id], function(error2, topic) {
                     if(error2) {
                         throw error2;
                     }
-                    console.log(topic);
                     var title = topic[0].title;
                     var description = topic[0].description;
                     var list = template.list(topics);
                     var html = template.HTML(title, list,
-                        `<h2>${title}</h2>
-                        ${description}
-                        <p>by ${topic[0].name}</p>
-                        `,
+                        `<h2>${title}</h2>${description}`,
                         ` <a href="/create">create</a>
                             <a href="/update?id=${queryData.id}">update</a>
                             <form action="delete_process" method="post">
