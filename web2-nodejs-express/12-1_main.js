0a09e1adc1e6873538cdcd941e13b05afc679862
metalrose.hong <metalrose.hong@gmail.com>
2020-01-31 01:10:16 +0900

    Add Chapter12
    Add Chapter12

--- web2-nodejs-express/12-1_main.js
+++ web2-nodejs-express/12-1_main.js
@@ -22,9 +22,7 @@ app.get('/', function(request, response) {
     var description = 'Hello, Node.js';
     var list = template.list(request.list);
     var html = template.HTML(title, list,
-        `<h2>${title}</h2>${description}
-        <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
-        `,
+        `<h2>${title}</h2>${description}`,
         `<a href="/create">create</a>`
     );
     response.send(html);
